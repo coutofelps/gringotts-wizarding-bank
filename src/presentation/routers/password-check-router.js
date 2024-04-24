358b87b93f9b650d21af0390f231def4ed0ba106
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-04-23 21:36:16 -0300

    :white_check_mark: test: ensure password check router calls PasswordCheckUseCase with correct params

--- src/presentation/routers/password-check-router.js
+++ src/presentation/routers/password-check-router.js
@@ -1,6 +1,10 @@
 const HttpReponse = require('../helpers/http-response')
 
 module.exports = class PasswordCheckRouter {
+  constructor (passworCheckUseCase) {
+    this.passworCheckUseCase = passworCheckUseCase
+  }
+
   route (httpRequest) {
     if (!httpRequest || !httpRequest.body) {
       return HttpReponse.serverError()
@@ -11,5 +15,7 @@ module.exports = class PasswordCheckRouter {
     if (!password) {
       return HttpReponse.badRequest('password')
     }
+
+    this.passworCheckUseCase.check(password)
   }
 }
--- src/presentation/routers/password-check-router.spec.js
+++ src/presentation/routers/password-check-router.spec.js
@@ -2,13 +2,25 @@ const PasswordCheckRouter = require('./password-check-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
 const makeSut = () => {
-  return new PasswordCheckRouter()
+  class PasswordCheckUseCaseSpy {
+    check (password) {
+      this.password = password
+    }
+  }
+
+  const passwordCheckUseCaseSpy = new PasswordCheckUseCaseSpy()
+  const sut = new PasswordCheckRouter(passwordCheckUseCaseSpy)
+
+  return {
+    sut,
+    passwordCheckUseCaseSpy
+  }
 }
 
 describe('Password checker router', () => {
   test('Should return 400 if no password is provided', () => {
     // sut = system under test
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -22,14 +34,14 @@ describe('Password checker router', () => {
   })
 
   test('Should return 500 if no httpRequest is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpReponse = sut.route()
     expect(httpReponse.statusCode).toBe(500)
   })
 
   test('Should return 500 if httpRequest has no body', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {}
 
@@ -38,6 +50,15 @@ describe('Password checker router', () => {
   })
 
   test('Should call PasswordCheckUseCase with correct params', () => {
+    const { sut, passwordCheckUseCaseSpy } = makeSut()
 
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+
+    sut.route(httpRequest)
+    expect(passwordCheckUseCaseSpy.password).toBe(httpRequest.body.password)
   })
 })
