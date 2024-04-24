a2e4fbdefddb64a442300950622f56c7a723ee77
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-04-24 20:03:21 -0300

    :white_check_mark: test: move sut creation to a factory helper method

--- src/presentation/utils/password-validator.spec.js
+++ src/presentation/utils/password-validator.spec.js
@@ -1,14 +1,27 @@
+const validator = require('validator')
+
 class PasswordValidator {
   isValid (password) {
-    return true
+    return validator.isStrongPassword(password)
   }
+}
+
+const makeSut = () => {
+  return new PasswordValidator()
 }
 
 describe('Password validator', () => {
   test('Should return true if validator returns true', () => {
-    const sut = new PasswordValidator()
+    const sut = makeSut()
     const isPasswordValid = sut.isValid('AbTp9!fok')
 
     expect(isPasswordValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    const sut = makeSut()
+    const isPasswordValid = sut.isValid('invalid_password')
+
+    expect(isPasswordValid).toBe(false)
   })
 })
