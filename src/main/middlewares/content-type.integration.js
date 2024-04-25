7fbbcec5f7dab5d43913d3265b6f5562d21557ec
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-04-24 22:07:10 -0300

    :white_check_mark: test: reset jest modules in content-type middleware

--- src/main/middlewares/content-type.integration.js
+++ src/main/middlewares/content-type.integration.js
@@ -1,25 +1,32 @@
 const request = require('supertest')
-const app = require('../config/app')
 
 describe('Content-Type Middleware', () => {
+  let app
+
+  // Reseting Jest URL cache (used if you want to use same URL for an integration test)
+  beforeEach(() => {
+    jest.resetModules()
+    app = require('../config/app')
+  })
+
   test('Should return JSON content-type', async () => {
-    app.get('/test_content_type_json', (req, res) => {
+    app.get('/test_content_type', (req, res) => {
       res.send('')
     })
 
     await request(app)
-      .get('/test_content_type_json')
+      .get('/test_content_type')
       .expect('content-type', /json/)
   })
 
   test('Should return XML as forced content-type', async () => {
-    app.get('/test_content_type_xml', (req, res) => {
+    app.get('/test_content_type', (req, res) => {
       res.type('text/xml')
       res.send('')
     })
 
     await request(app)
-      .get('/test_content_type_xml')
+      .get('/test_content_type')
       .expect('content-type', /xml/)
   })
 })
