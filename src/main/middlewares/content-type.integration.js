0e49f14902f23024e08957983e603589a5fe80c9
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-04-24 22:01:24 -0300

    :white_check_mark: test: should return XML as forced content-type

--- src/main/middlewares/content-type.integration.js
+++ src/main/middlewares/content-type.integration.js
@@ -2,13 +2,24 @@ const request = require('supertest')
 const app = require('../config/app')
 
 describe('Content-Type Middleware', () => {
-  test('Should return JSON content type', async () => {
-    app.get('/test_content_type', (req, res) => {
+  test('Should return JSON content-type', async () => {
+    app.get('/test_content_type_json', (req, res) => {
       res.send('')
     })
 
     await request(app)
-      .get('/test_content_type')
+      .get('/test_content_type_json')
       .expect('content-type', /json/)
+  })
+
+  test('Should return XML as forced content-type', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('text/xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
